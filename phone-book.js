927d634ac82f76ed4b3e167bd13d10f7ba7fcc69
defntvdm <defntvdm@gmail.com>
2017-10-05 20:52:15 +0500

    Проверка начала номера на +

--- phone-book.js
+++ phone-book.js
@@ -13,7 +13,8 @@ var phoneBook = [];
 
 function validData(phone, name) {
     const intPhone = Number(phone);
-    if (isNaN(intPhone) || phone.length !== 10 || name === undefined || name === '') {
+    if (isNaN(intPhone) || phone.length !== 10 || name === undefined || name === '' ||
+        phone[0] === '+') {
         return false;
     }
 
