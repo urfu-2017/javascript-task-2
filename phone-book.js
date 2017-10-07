2e4b9d93640b003de440d1f7e53dbfde4249057c
Behemothhh <elakov111@gmail.com>
2017-10-07 23:55:07 +0500

    lets try

--- phone-book.js
+++ phone-book.js
@@ -18,7 +18,13 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
-
+    if (!/[0-9]{10}/.test(phone) && phone.length !== 10 || !name || !checkEmail(email) ) {
+        return false;
+    }
+    phoneBook.push({phone,name});
+    if (email) {
+        phoneBook[length-1].email = email;
+    }
 };
 
 /**
@@ -28,7 +34,16 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
-
+    let index = find(Object.values(phoneBook), phone);
+    if (index === -1 || !name || (email && !checkEmail(email))) {
+        return false;
+    }
+    phoneBook[index].name = name;
+    if (email) {
+        phoneBook[inde].email = email;
+    } else {
+        delete phoneBook[index].email;
+    }
 };
 
 /**
@@ -36,7 +51,17 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
-
+    let countDelete = 0;
+    for (var i = phoneBook.length - 1; i >= 0 ; i--) {
+        for (let value of phoneBook[i]) {
+            if (value.indexOf(query) !== -1) {
+                phoneBook.splice(i, 1);
+                countDelet++;
+                break;
+            }
+        }
+    }
+    return countDelete;
 };
 
 /**
@@ -44,7 +69,22 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
-
+    let resuslt = [];
+    for (let person of phoneBook) {
+        result.push('');
+        for (let value of Object.values(person)) {
+            if (value.indexOf(query) !== -1) {
+                result.push(person.name + ', +7 (' + person.phone.slice(0, 3) + ') ' +
+                person.phone.slice(3, 6) + '-' + person.phone.slice(6, 8) + '-' +
+                person.phone.slice(8));
+                if (person.email) {
+                    result[result.length - 1] += ', ' + person.email;
+                }
+                break;
+            }
+        }
+    }
+    return result;
 };
 
 /**
@@ -57,6 +97,20 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
-
-    return csv.split('\n').length;
+    let countUpdates = 0;
+    let arrUsers = csv. split('\n');
+    for (let user of arrUsers) {
+        let userSplit = user.split(';');
+        if(find(userSplit[1])) {
+            update(userSplit[1], userSplit[0], userSplit[2]);
+            countUpdates++;
+        } else {
+            add(userSplit[1], userSplit[0], userSplit[2])
+        }
+    }
+    return countUpdates;
 };
+
+function checkEmail(query) {
+    return /[a-z0-9-]+@[a-z0-9-]+\.[a-z0-9-]/i.test(query);
+}
