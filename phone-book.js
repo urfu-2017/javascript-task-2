dee583183758d2727c9a3fc57e32a132c5a3aea4
spt30 <seregatoschilin@gmail.com>
2017-10-08 05:47:57 +0500

    Убрал пару лишних проверок

--- phone-book.js
+++ phone-book.js
@@ -62,7 +62,7 @@ exports.update = function (phone, name, email) {
         //                                      console.log('update', phoneBook[i].p, phone, i);
         ++i;
     }
-    if (i !== phoneBook.length /* && phoneBook[i].p === phone*/ && name) {
+    if (i !== phoneBook.length && name) {
         phoneBook[i].n = name;
         if (email) {
             phoneBook[i].m = email;
