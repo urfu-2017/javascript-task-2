2010bd9efd67dc68548f43ef229041929856e403
chrighter <christina.ilyenko@yandex.ru>
2017-10-08 17:41:35 +0500

    ver 1

--- index.js
+++ index.js
@@ -3,33 +3,53 @@
 var phoneBook = require('./phone-book');
 
 // Эти записи добавятся, вернется true
-phoneBook.add('5554440044', 'Григорий', 'grisha@example.com');
-phoneBook.add('5552220022', 'Борис', 'boris@example.com');
-phoneBook.add('5551110011', 'Алекс');
-phoneBook.add('5553330033', 'Валерий', 'valera@example.com');
+console.info(phoneBook.add('5554440044', 'Григорий', 'grisha@example.com'));
+console.info(phoneBook.add('5512220022', 'Борис', 'bori$s@example.com'));
+console.info(phoneBook.add('5551110011', 'Алекс'));
+console.info(phoneBook.add('5553333332', 'Михаил', 'Misha@example.com'));
+console.info(phoneBook.add('5553330033', 'Валерий', 'valera@example.com'));
+console.info(phoneBook.add('5553333333', 'Валерий', 'valerii@example.com'));
+console.info(phoneBook.add('5553333331', 'Яков', 'Yakov@example.com'));
 
-// Эти запись не добавятся
-phoneBook.add('3330033', 'Неизвестный', 'unknown@example.com');
-phoneBook.add('5551110011', 'Алексей');
-phoneBook.add('5555550055');
+// // Эти запись не добавятся
+// phoneBook.add('3330033', 'Неизвестный', 'unknown@example.com');
+// phoneBook.add('5551110011', 'Алексей');
+// phoneBook.add('5555550055');
+
+// console.info(phoneBook.add('8800110011', '', 'alex@example.com'));
+// console.info(phoneBook.add('3330033', 'Неизвестный', 'unknown@example.com'));
+// console.info(phoneBook.add('5551110011', 'Алексей'));
+// console.info(phoneBook.add('5555550055'));
+// console.info(phoneBook.add('5553333333', 'Валерий', 'valerii@example.com'));
+
 
 // Обновление
-phoneBook.update('5551110011', 'Алексей', 'alex@example.com');
-phoneBook.update('5553330033', 'Валерий');
+// console.info(phoneBook.update('5551110011', 'Леха', 'alex@example.com'));
+// console.info(phoneBook.update('5553330033', 'Валерий'));
+// console.info(phoneBook.update('5553332233', undefined, 'withoutName@example.com'));
+// console.info(phoneBook.update('5553332233', ' ', 'withoutName@example.com'));
+// console.info(phoneBook.update('5553332233', '     ', 'withoutName@example.com'));
 
-// В следующих примерах вернутся все записи
-console.info(phoneBook.find('*'));
-console.info(phoneBook.find('555'));
-// Вывод будет следующий
-// [
-//   'Алексей, +7 (555) 111-00-11, alex@example.com',
-//   'Борис, +7 (555) 222-00-22, boris@example.com',
-//   'Валерий, +7 (555) 333-00-33',
-//   'Григорий, +7 (555) 444-00-44, grisha@example.com'
-// ]
+// пустой
+// console.info(phoneBook.find('kek'));
+// console.info(phoneBook.find(''));
+// // В следующих примерах вернутся все записи
+// console.info(phoneBook.find('555'));
 
-// Удаление
-phoneBook.findAndRemove('@'); // returns 3
+// console.info(phoneBook.find(''));
+// console.info(phoneBook.find('Неизвестный'));
+// console.info(phoneBook.find('*'));
+// console.info(phoneBook.find('@'));
+// // Вывод будет следующий
+// // [
+// //   'Алексей, +7 (555) 111-00-11, alex@example.com',
+// //   'Борис, +7 (555) 222-00-22, boris@example.com',
+// //   'Валерий, +7 (555) 333-00-33',
+// //   'Григорий, +7 (555) 444-00-44, grisha@example.com'
+// // ]
+
+// // Удаление
+// phoneBook.findAndRemove('@'); // returns 6
 
 if (phoneBook.isStar) {
     // Импортируем из csv
--- phone-book.js
+++ phone-book.js
@@ -9,16 +9,54 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+let phoneBook = {};
+exports.phoneBook = phoneBook;
+
+function checkInputCorrect(phone, name, email) {
+    return checkName(name) && checkPhone(phone) && checkEmail(email) && !(phone in phoneBook);
+}
+
+function checkName(name) {
+    return name !== 'Неизвестный' && name !== undefined && /^[A-Za-zА-Яа-я]+$/.test(name);
+}
+
+function checkEmail(email) {
+    if (email !== undefined) {
+        return /^[a-zA-Z0-9.^$*!-=`|~#%'+/?_{}]+@[a-zA-Z0-9.]+\.[a-zA-Z]+$/.test(email);
+    }
+
+    return true;
+}
+
+function checkPhone(phone) {
+    return phone !== undefined && /[0-9]{10}/.test(phone);
+}
+
+// На вход принимает «Телефон», «Имя» и «Электронную почту»
+// Возвращает true или false в зависимости от успеха опереации
+// Телефоны принимаются **только** в формате 5556667788 (без кода)
+// Не добавляет **уже существующую** запись
+// Не добавляет запись **без имени**
 
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Dict}
  */
 exports.add = function (phone, name, email) {
+    if (checkInputCorrect(phone, name, email)) {
+        if (email === undefined) {
+            phoneBook[phone] = `${phone}, ${name}`;
+        } else {
+            phoneBook[phone] = `${phone}, ${name}, ${email}`;
+        }
 
+        return true;
+    }
+
+    return false;
 };
 
 /**
@@ -27,24 +65,91 @@ exports.add = function (phone, name, email) {
  * @param {String} name
  * @param {String} email
  */
+// На вход принимает «Телефон», «Имя» и «Электронную почту»
+// Обновляет «Имя» и «Электронную почту» по заданному «Телефону»
+// Возвращает true или false в зависимости от успеха опереации
+// «Электронную почту» можно стереть (не передав последний параметр), а «Имя» – нет
 exports.update = function (phone, name, email) {
+    if (checkName(name) && phone in phoneBook && checkEmail(email)) {
+        if (email !== undefined) {
+            phoneBook[phone] = `${phone}, ${name}, ${email}`;
+        } else {
+            phoneBook[phone] = `${phone}, ${name}`;
+        }
 
+        return true;
+    }
+
+    return false;
 };
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
  */
+// На вход принимает запрос в виде строки
+// Находит (смотри __find__) и удаляет все найденные записи
+// Возвращает число удаленных записей
 exports.findAndRemove = function (query) {
+    let foundEntries = exports.find(query);
+    let countDeleted = 0;
+    foundEntries.forEach(entry => {
+        delete phoneBook[entry.split(', ')[1]];
+        countDeleted += 1;
+    });
 
+    return countDeleted;
 };
+
+function searchSubstring(query, entries) {
+    let foundEntries = [];
+    if (query !== '*') {
+        foundEntries = entries.filter(entry => entry.match(query));
+    } else {
+        foundEntries = entries;
+    }
+
+    return foundEntries;
+}
+
+function formatPhone(phone) {
+    return `+7 (${phone.slice(0, 3)}) ${phone.slice(3, 6)}-${phone.slice(6, 8)}-${phone.slice(8)}`;
+}
+
+function formFoundedData(foundEntries) {
+    let foundedStrings = [];
+    foundEntries.forEach(item => {
+        let entries = item.split(', ');
+        if (entries[2] !== undefined) {
+            foundedStrings.push(`${entries[1]}, ${formatPhone(entries[0])}, ${entries[2]}`);
+        } else {
+            foundedStrings.push(`${entries[1]}, ${formatPhone(entries[0])}`);
+        }
+    });
+
+    return foundedStrings;
+}
+
+// На вход принимает запрос в виде строки
+// Ищет вхождение этой строки хотя бы в одно из полей «Телефон», «Имя» и «Электронную почту»
+// Возвращает отсортированный по «Имени» массив строк в формате `name, phone, email`
+// «Имя» и «Электронную почту» выводит как есть, а «Телефон» в формате `+7 (555) 666-77-88`
+// Пустой запрос не должен ничего находить
+// Запрос «*» находит все записи
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
+ * @returns {Array}
  */
 exports.find = function (query) {
+    if (query === '') {
+        return [];
+    }
+    let entries = Object.values(phoneBook);
+    let foundEntries = searchSubstring(query, entries);
 
+    return formFoundedData(foundEntries).sort();
 };
 
 /**
@@ -57,6 +162,18 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    let data = csv.split('\n');
+    let countBefore = Object.keys(phoneBook).length;
+    let countUpdate = 0;
+    data.forEach(item => {
+        let entriesArray = item.split(';');
+        if (!exports.add(entriesArray[1], entriesArray[0], entriesArray[2])) {
+            if (exports.update(entriesArray[1], entriesArray[0], entriesArray[2])) {
+                countUpdate += 1;
+            }
+        }
+    });
+    let countAfter = Object.keys(phoneBook).length;
 
-    return csv.split('\n').length;
+    return countAfter - countBefore + countUpdate;
 };
