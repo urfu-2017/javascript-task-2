3d2d8ad5d8f08af0bef786e181cd2fcd3040e42f
spt30 <seregatoschilin@gmail.com>
2017-10-08 22:59:43 +0500

    Добавил LowerCase в F&R для name

--- phone-book.js
+++ phone-book.js
@@ -124,6 +124,7 @@ function checkInclude(i, query) {
     let b1 = false;
     var a = [];
     a[0] = phoneBook[i].n.toLowerCase();
+    a[3] = phoneBook[i].n;
     //  a[0] = a[0].toLowerCase();
     a[1] = phoneBook[i].p;
     a[2] = phoneBook[i].m || '';
@@ -132,7 +133,7 @@ function checkInclude(i, query) {
     for (let value of values) {
     console.log(value, '\n', String(value), '\n',
     String(value).indexOf(query), '\n', query);*/
-    for (let x = 0; x <= 2; x += 1) {
+    for (let x = 0; x <= 3; x += 1) {
         if (String(a[x]).indexOf(query) + 1) {
             b1 = true;
         }
