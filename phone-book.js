9ec18db5f21207584f23aa3c548f5b4a0d5367e6
danmerus <leoghost@mail.ru>
2017-10-09 10:56:48 +0500

    Happy first commit

--- phone-book.js
+++ phone-book.js
@@ -4,23 +4,59 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Телефонная книга
  */
-var phoneBook;
-
+var phoneBook = [];
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
  */
-exports.add = function (phone, name, email) {
-
+function checkIfName(name) {
+    var reg = new RegExp('@');
+    return !reg.test(name);
+}
+function formatData(data) {
+    var res = [];
+    for (var index = 0; index < data.length; ++index) {
+        var tempStr = "";
+        if ((typeof data[index].email != 'undefined')) {
+            tempStr = data[index].name + ", " + numberFormater(data[index].phone)+ ", "
+                + data[index].email;
+        } else {
+            tempStr = data[index].name + ", " + numberFormater(data[index].phone)
+        }
+        res.push(tempStr);
+    }
+    res = res.sort();
+    return res;
+}
+function numberFormater(number) {
+    return "+7 (" + number.substring(0,3)+") " +
+        number.substring(3,6) + "-" + number.substring(6,8) + "-" +
+        number.substring(8,10)
+}
+exports.add = function(phone, name, email) {
+    var reg = new RegExp('^[0-9]{10}$');
+    var entry = {phone: phone, name: name, email: email};
+    if (reg.test(parseInt(phone)) && (typeof name != 'undefined')
+        && checkIfName(name)){
+        for (var index = 0; index < phoneBook.length; ++index) {
+            if ((typeof phoneBook[index].phone != 'undefined')
+                && phoneBook[index].phone.localeCompare(phone) == 0) {
+               return false;
+            }
+        }
+        phoneBook.push(entry);
+        return true;
+    } else {
+        return false;
+    }
 };
-
 /**
  * Обновление записи в телефонной книге
  * @param {String} phone
@@ -28,15 +64,51 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
-
+    var entry = {phone: phone, name: name, email: email};
+    var reg = new RegExp('^[0-9]{10}$');
+    if (reg.test(parseInt(phone)) && (typeof name != 'undefined')
+        && checkIfName(name)){
+        for (var index = 0; index < phoneBook.length; ++index) {
+            if ((typeof phoneBook[index].phone != 'undefined')
+                && phoneBook[index].phone.localeCompare(phone) == 0) {
+                phoneBook[index] = entry;
+                return true;
+            }
+        }
+        return false;
+    } else {
+        return false;
+    }
 };
+
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
-
+    var out = [];
+    if (query == ''){
+        return 0;
+    }
+    if (query == '*'){
+        phoneBook = [];
+        return phoneBook.length;
+    }
+    for (var index = 0; index < phoneBook.length; ++index) {
+        for ( var k in phoneBook[index] ) {
+            if (phoneBook[index].hasOwnProperty(k) &&
+                (typeof phoneBook[index][k] != 'undefined') &&
+                phoneBook[index][k].indexOf(query) != -1){
+                out.push(phoneBook[index]);
+            }
+        }
+    }
+    for (var i= 0; index < out.length; ++i) {
+        var ind = phoneBook.indexOf(out[i]);
+        phoneBook.splice(ind, 1);
+    }
+    return out.length;
 };
 
 /**
@@ -44,9 +116,27 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
-
+    var out = [];
+    if (query == ''){
+        return
+    }
+    if (query == '*'){
+        return formatData(phoneBook);
+    }
+    for (var index = 0; index < phoneBook.length; ++index) {
+        for ( var k in phoneBook[index] ) {
+            if (phoneBook[index].hasOwnProperty(k) &&
+                (typeof phoneBook[index][k] != 'undefined') &&
+                phoneBook[index][k].indexOf(query) != -1){
+                out.push(phoneBook[index]);
+            }
+        }
+    }
+    if (out.length != 0) {
+        return formatData(out);
+    }
+    return "";
 };
-
 /**
  * Импорт записей из csv-формата
  * @star
