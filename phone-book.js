22a692736007439265c2f9c4251cebfe0ea1e1d2
valerin5 <valera9719@gmail.com>
2017-10-11 03:02:11 +0500

    раз

--- phone-book.js
+++ phone-book.js
@@ -3,21 +3,40 @@
 /**
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
- */
+ *  */
 exports.isStar = true;
 
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
-/**
- * Добавление записи в телефонную книгу
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
-exports.add = function (phone, name, email) {
+function validData(phone, name) {
+    if (!/^\d{10}$/.test(String(phone)) || name === '') {
+        return false;
+    }
+}
+
+exports.add = function (phon, nam, email) {
+    if (!validData(phon, nam)) {
+        return false;
+    }
+    for (var i = 0; i < phoneBook.length; i++) {
+        if
+        (phoneBook[i].phone === phon || phoneBook[i].name === nam || phoneBook[i].email === email) {
+
+            return false;
+
+        }
+    }
+    var newContact = {
+        phone: phon,
+        name: nam,
+        email: email
+    };
+    phoneBook.push(newContact);
+
+    return true;
 
 };
 
@@ -27,23 +46,23 @@ exports.add = function (phone, name, email) {
  * @param {String} name
  * @param {String} email
  */
-exports.update = function (phone, name, email) {
+/** exports.update = function (phone, name, email) {
 
-};
+};/
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
  */
-exports.findAndRemove = function (query) {
+/** exports.findAndRemove = function (query) {
 
-};
+};/
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
  */
-exports.find = function (query) {
+/** exports.find = function (query) {/
 
 };
 
