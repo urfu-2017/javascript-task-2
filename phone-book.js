6af5baa6c9be8cb123760e5aee79f17beea90a5d
Evgeny Markov <evgenymarkov@yandex.ru>
2017-10-12 01:22:52 +0500

    Simplify phone formatter

--- phone-book.js
+++ phone-book.js
@@ -171,9 +171,6 @@ function phoneEntryComparer(first, second) {
  * @returns {String} phone
  */
 function formatPhone(phone) {
-    return '+7 (' +
-        phone.slice(0, 3) + ') ' +
-        phone.slice(3, 6) + '-' +
-        phone.slice(6, 8) + '-' +
-        phone.slice(8, 10);
+    return `+7 (${phone.slice(0, 3)}) ${phone.slice(3, 6)}-` +
+        `${phone.slice(6, 8)}-${phone.slice(8, 10)}`;
 }
