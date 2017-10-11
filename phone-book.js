384964fbdaad0bad99ba6d53343d763e04e8d4b7
reindeermur <sofiabochik@gmail.com>
2017-10-11 19:42:28 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -1,82 +1,57 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализован метод importFromCsv
- */
 exports.isStar = true;
 
-/**
- * Телефонная книга
- */
 var phoneBook = {};
 var phoneReg = new RegExp('^[0-9]{10}$');
 
-/**
- * @return {boolean}
- */
-function CheckPhone(number) {
-    return !!phoneReg.exec(number);
-
-}
-/**
- * @return {boolean}
- */
-function CheckName(name) {
-    return !!(name !== ' ' && name !== undefined && name !== null);
-
+function checkPhone(number) {
+    if (phoneReg.exec(number)) {
+        return true;
+    }
+    return false;
 }
 
-/**
- * @return {string}
- */
+function checkName(name) {
+    if (name !== ' ' && name !== undefined && name !== null) {
+        return true;
+    }
+    return false;
+
+}
+
 function TransformPhone(number) {
-    return ('+7 (' + number.slice(0, 3) + ') ' + number.slice(3, 6)
-    + '-' + number.slice(6, 8) + '-' + number.slice(8, 10));
+    return ('+7 (' + number.slice(0, 3) + ') ' + number.slice(3, 6) +
+    '-' + number.slice(6, 8) + '-' + number.slice(8, 10));
 }
 
-/**
- * @return {boolean}
- */
-function CheckRecordByPhone(number) {
-    return !!phoneBook[number];
-
+function checkRecordByPhone(number) {
+    if (phoneBook[number]) {
+        return true;
+    }
+    return false;
 }
-
-/**
- * Добавление записи в телефонную книгу
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- * @return {boolean}
- */
 
 exports.add = function (phone, name, email) {
-    if (CheckPhone(phone) && CheckName(name) && !CheckRecordByPhone(phone)) {
-        phoneBook[phone] = {name, email};
+    if (checkPhone(phone) && checkName(name) && !checkRecordByPhone(phone)) {
+        phoneBook[phone] = { name, email };
+
         return true;
     }
+
     return false;
 };
 
-/**
- * Обновление записи в телефонной книге
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
 exports.update = function (phone, name, email) {
-    if (CheckRecordByPhone) {
-        phoneBook[phone] = {name, email};
+    if (checkRecordByPhone) {
+        phoneBook[phone] = { name, email };
+
         return true;
     }
+
     return false;
 };
 
-/**
- * Удаление записей по запросу из телефонной книги
- * @param {String} query
- */
 exports.findAndRemove = function (query) {
     let deleted = [];
     if (query === '*') {
@@ -95,10 +70,6 @@ exports.findAndRemove = function (query) {
 
 };
 
-/**
- * Поиск записей по запросу в телефонной книге
- * @param {String} query
- */
 exports.find = function (query) {
     let result = [];
     let records = findRecords(query);
@@ -110,6 +81,7 @@ exports.find = function (query) {
         contacts.push(records[phone].email);
         result.push(contacts.join(', '))
     }
+
     return result.sort();
 
 };
@@ -120,50 +92,45 @@ function findRecords(query) {
         result = phoneBook;
     }
     if (query !== '') {
-        result = RecordsInBook(query)
+        result = recordsInBook(query)
     } else {
         result = {};
     }
+
     return result;
 }
 
-
-function RecordsInBook(query) {
+function recordsInBook(query) {
     let records = {};
     for (let contact of Object.keys(phoneBook)) {
-        if (CheckRecord(phoneBook[contact], query) || contact.indexOf(query)) {
+        if (checkRecord(phoneBook[contact], query) || contact.indexOf(query)) {
             records[contact] = {
                 name: phoneBook[contact].name,
-                email: phoneBook[contact].email
-            };
+                email: phoneBook[contact].email };
         }
     }
+
     return records;
 }
 
-/**
- * @return {boolean}
- */
-function CheckRecord(record, query) {
-    return !!((record.name.indexOf(query))
-    || (record.email.indexOf(query)));
+function checkRecord(record, query) {
+    if ((record.name.indexOf(query)) ||
+        (record.email.indexOf(query))){
+
+        return true;
+    }
+
+    return false;
 }
-/**
- * Импорт записей из csv-формата
- * @star
- * @param {String} csv
- * @returns {Number} – количество добавленных и обновленных записей
- */
+
 exports.importFromCsv = function (csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
     let records = csv.split('\n');
     let updateRecords = 0;
     for (const [name, phone, email] of records) {
         if (exports.update(phone, name, email) || exports.add(phone, name, email)) {
-            updateRecords ++;
+            updateRecords++;
         }
     }
+
     return updateRecords;
 };
