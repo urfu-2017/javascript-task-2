5ebf59e61fa5d010f86d879640a9d8a32e7a4043
GalinaRoma <kisaga@mail.ru>
2017-10-11 22:11:44 +0500

    Первое решение этой задачи

--- phone-book.js
+++ phone-book.js
@@ -4,59 +4,146 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} result
  */
 exports.add = function (phone, name, email) {
+    let added = false;
+    if (isNameCorrect(phone, name) && !(phone in phoneBook)) {
+        phoneBook[phone] = { name: name, email: email };
+        added = true;
+    }
 
+    return added;
 };
+
+function isNameCorrect(phone, name) {
+    let regExp = /[0-9]{10}/;
+
+    return (name !== '' && name !== null && name !== undefined &&
+    phone.length === 10 && regExp.test(phone));
+}
 
 /**
  * Обновление записи в телефонной книге
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} result
  */
 exports.update = function (phone, name, email) {
+    let updated = false;
+    if (phone in phoneBook && isNameCorrect(phone, name)) {
+        phoneBook[phone] = { name: name, email: email };
+        updated = true;
+    }
 
+    return updated;
 };
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
+ * @returns {Boolean}
  */
 exports.findAndRemove = function (query) {
+    let countDeleted;
+    if (query === '') {
+        countDeleted = 0;
+    } else if (query === '*') {
+        countDeleted = Object.keys(phoneBook).length;
+        for (let phone of Object.keys(phoneBook)) {
+            delete phoneBook[phone];
+        }
+    } else {
+        let linesToDelete = searchLines(query);
+        countDeleted = Object.keys(linesToDelete).length;
+        for (let phone of Object.keys(phoneBook)) {
+            delete phoneBook[phone];
+        }
+    }
 
+    return countDeleted;
 };
+
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
+ * @returns {Boolean} result
  */
 exports.find = function (query) {
+    let results = [];
+    if (query === '') {
+        results = [];
+    } else if (query === '*') {
+        results = makeResultList(phoneBook);
+    } else {
+        let resultOfSearch = searchLines(query);
+        results = makeResultList(resultOfSearch);
+    }
 
+    return results;
 };
 
-/**
- * Импорт записей из csv-формата
- * @star
- * @param {String} csv
- * @returns {Number} – количество добавленных и обновленных записей
- */
-exports.importFromCsv = function (csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
 
-    return csv.split('\n').length;
-};
+function makeResultList(inputObject) {
+    let finalResults = [];
+    for (let phone of Object.keys(inputObject)) {
+        let finalCurrentString = '';
+        finalCurrentString += inputObject[phone].name + ', ';
+        finalCurrentString += createPhoneView(phone);
+        if (inputObject[phone].email !== undefined && inputObject[phone].email !== null) {
+            finalCurrentString += ', ' + inputObject[phone].email;
+        }
+        finalResults.push(finalCurrentString);
+    }
+    finalResults = finalResults.sort();
+
+    return finalResults;
+}
+
+function createPhoneView(phone) {
+    let updatedPhone = phone;
+    let regExp = /(\d{3})(\d{3})(\d{2})(\d{2})/;
+
+    return updatedPhone.replace(regExp, '+7 ($1) $2-$3-$4');
+}
+
+function searchLines(query) {
+    let results = {};
+    let phones = Object.keys(phoneBook);
+    for (let phone of phones) {
+        if (phone.indexOf(query) !== -1 || phoneBook[phone].name.indexOf(query) !== -1 ||
+        (phoneBook[phone].email !== undefined && phoneBook[phone].email.indexOf(query) !== -1)) {
+            results[phone] = phoneBook[phone];
+        }
+    }
+
+    return results;
+}
+
+// /**
+//  * Импорт записей из csv-формата
+//  * @star
+//  * @param {String} csv
+//  * @returns {Number} – количество добавленных и обновленных записей
+//  */
+// exports.importFromCsv = function (csv) {
+//     // Парсим csv
+//     // Добавляем в телефонную книгу
+//     // Либо обновляем, если запись с таким телефоном уже существует
+
+//     return csv.split('\n').length;
+// };
