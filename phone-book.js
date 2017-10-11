3b31bb09ec9205af2dcf6a8da490d39998154a5f
dru <denginru@yandex.ru>
2017-10-11 23:30:11 +0500

    one of wrong csv line format type corrected

--- phone-book.js
+++ phone-book.js
@@ -138,6 +138,9 @@ exports.importFromCsv = function (csv) {
     var k = 0;
     var lines = csv.split('\n');
     for (let i = 0; i < lines.length; i += 1) {
+        if (oneString.length !== 2 && oneString.length !== 3) {
+            continue;
+        }
         var line = lines[i].split(';');
         if (exports.add(line[1], line[0], line[2])) {
             k++;
