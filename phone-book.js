704fe929d4f4a138eef795996e5dcc2a99ddbb7b
Evgeny Markov <evgenymarkov@yandex.ru>
2017-10-12 01:09:40 +0500

    Done

--- phone-book.js
+++ phone-book.js
@@ -9,16 +9,26 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+let phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} true если запись успешно добавлена, иначе false
  */
 exports.add = function (phone, name, email) {
+    if (validatePhone(phone) && validateName(name)) {
+        if (phone in phoneBook) {
+            return false;
+        }
+        phoneBook[phone] = { 'name': name, 'email': email };
 
+        return true;
+    }
+
+    return false;
 };
 
 /**
@@ -26,25 +36,59 @@ exports.add = function (phone, name, email) {
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} true если запись успешно обновлена, иначе false
  */
 exports.update = function (phone, name, email) {
+    if (validatePhone(phone) && validateName(name)) {
+        phoneBook[phone] = { 'name': name, 'email': email };
 
+        return true;
+    }
+
+    return false;
 };
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
+ * @returns {Number} число удалённых записей
  */
 exports.findAndRemove = function (query) {
+    let matchedPhones = getMatches(query);
 
+    for (let phone of matchedPhones) {
+        delete matchedPhones[phone];
+    }
+
+    return matchedPhones.length;
 };
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
+ * @returns {Array}
  */
 exports.find = function (query) {
+    if (typeof query !== 'string') {
+        return [];
+    }
 
+    let matchedPhones;
+    if (query === '*') {
+        matchedPhones = Object.keys(phoneBook);
+    } else {
+        matchedPhones = getMatches(query);
+    }
+
+    matchedPhones = matchedPhones.sort(phoneEntryComparer);
+
+    let entries = [];
+    for (let phone of matchedPhones) {
+        let emailPart = phoneBook[phone].email ? `, ${phoneBook[phone].email}` : '';
+        entries.push(`${phoneBook[phone].name}, ${formatPhone(phone)}` + emailPart);
+    }
+
+    return entries;
 };
 
 /**
@@ -54,9 +98,82 @@ exports.find = function (query) {
  * @returns {Number} – количество добавленных и обновленных записей
  */
 exports.importFromCsv = function (csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
+    let stringEntries = csv.split('\n');
+    let entriesAddedCount = 0;
 
-    return csv.split('\n').length;
+    for (let stringEntry of stringEntries) {
+        let [name, phone, email] = stringEntry.split(';');
+        if (exports.add(phone, name, email) || exports.update(phone, name, email)) {
+            entriesAddedCount++;
+        }
+    }
+
+    return entriesAddedCount;
 };
+
+/**
+ * Проверка номера телефона
+ * @param {String} phone
+ * @returns {Boolean} true если номер телефона записан корректно, иначе false
+ */
+function validatePhone(phone) {
+    return typeof(phone) === 'string' && /^\d{10}$/.test(phone);
+}
+
+/**
+ * Проверка имени
+ * @param {String} name
+ * @returns {Boolean} true если имя прошло проверку, иначе false
+ */
+function validateName(name) {
+    return typeof(name) === 'string' && !name.startsWith(' ') && !name.endsWith(' ');
+}
+
+/**
+ * Получение списка записей телефонной книги удовлетворяющих запросу
+ * @param {String} query
+ * @returns {Array}
+ */
+function getMatches(query) {
+    let matchedKeys = [];
+
+    for (let phone in phoneBook) {
+        if (!phoneBook.hasOwnProperty(phone)) {
+            continue;
+        }
+
+        let isPhoneMatched = phone.includes(query);
+        let isNameMatched = phoneBook[phone].name.includes(query);
+        let isEmailMatched = phoneBook[phone].email && phoneBook[phone].email.includes(query);
+
+        if (isPhoneMatched || isNameMatched || isEmailMatched) {
+            matchedKeys.push(phone);
+        }
+    }
+
+    return matchedKeys;
+}
+
+function phoneEntryComparer(first, second) {
+    if (phoneBook[first].name > phoneBook[second].name) {
+        return 1;
+    }
+    if (phoneBook[first].name < phoneBook[second].name) {
+        return -1;
+    }
+
+    return 0;
+}
+
+/**
+ * Приводит телефон к формату +7 (555) 666-77-88 из формата 5556667788
+ * @param {String} phone
+ * @returns {String} phone
+ */
+function formatPhone(phone) {
+    return '+7 (' +
+        phone.slice(0, 3) + ') ' +
+        phone.slice(3, 6) + '-' +
+        phone.slice(6, 8) + '-' +
+        phone.slice(8, 10);
+}
