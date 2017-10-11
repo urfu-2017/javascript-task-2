e59bf89bedaa9b3e911e1843c6b625e4082231d3
dreamzoul <darcouc@mail.ru>
2017-10-12 03:44:06 +0500

    Мне же страшно...

--- index.js
+++ index.js
@@ -3,23 +3,38 @@
 var phoneBook = require('./phone-book');
 
 // Эти записи добавятся, вернется true
-phoneBook.add('5554440044', 'Григорий', 'grisha@example.com');
-phoneBook.add('5552220022', 'Борис', 'boris@example.com');
-phoneBook.add('5551110011', 'Алекс');
-phoneBook.add('5553330033', 'Валерий', 'valera@example.com');
+console.log('add +');
+console.info(phoneBook.add('5562220022', 'Григорий', 'grisha@example.com'));//T
+console.info(phoneBook.add('5552220022', 'Борис', 'boris@example.com'));//T
+console.info(phoneBook.add('jhgkhgf', 'Григорий', 'grisha@example.com'));//F
+console.info(phoneBook.add('115552220022', 'Борис', 'boris@example.com'));//F
+console.info(phoneBook.add('5110011', 'Алекс'));//F
+console.info(phoneBook.add('5551110011', 'Алексей'));//t
+// console.info(phoneBook.add(, , 'valera@example.com'));//F
+console.info(phoneBook.add('', 'Валерий1', 'valera@example.com'));//F
+console.info(phoneBook.add('5553330033', 'Валерий', null));//t
+
 
 // Эти запись не добавятся
-phoneBook.add('3330033', 'Неизвестный', 'unknown@example.com');
-phoneBook.add('5551110011', 'Алексей');
-phoneBook.add('5555550055');
+console.log('add -');
+console.info(phoneBook.add('3330033', 'Неизвестный', 'unknown@example.com'));//F
+console.info(phoneBook.add('5551110011', 'Алексей'));//f
+console.info(phoneBook.add('5555550055'));//F
 
 // Обновление
-phoneBook.update('5551110011', 'Алексей', 'alex@example.com');
-phoneBook.update('5553330033', 'Валерий');
+console.log('up +');
+console.info(phoneBook.update('5551110011', 'Алексей', 'alex@example.com'));//T
+console.info(phoneBook.update('5553330033', '', 'alex111@example.com'));//F
+console.info(phoneBook.update(null, 'Валерий',));//F
+console.info(phoneBook.update('5553330033', 'Валерий'));//T
 
 // В следующих примерах вернутся все записи
+console.log('FIND');
 console.info(phoneBook.find('*'));
 console.info(phoneBook.find('555'));
+console.info(phoneBook.find(''));
+console.info(phoneBook.find(null));
+console.info(phoneBook.find(undefined));
 // Вывод будет следующий
 // [
 //   'Алексей, +7 (555) 111-00-11, alex@example.com',
@@ -29,7 +44,16 @@ console.info(phoneBook.find('555'));
 // ]
 
 // Удаление
-phoneBook.findAndRemove('@'); // returns 3
+console.log('DELETE');
+console.info(phoneBook.find('*'));
+console.info(phoneBook.findAndRemove('Валерий'));//1
+phoneBook.add('4562220022', 'Бориска', 'boris@example.com');
+console.info(phoneBook.find('*'));
+console.info(phoneBook.findAndRemove('рис'));//2
+//console.info(phoneBook.findAndRemove('@')); //2
+console.info(phoneBook.find('*'));
+console.info(phoneBook.findAndRemove('*'));//2
+console.info(phoneBook.find('*'));
 
 if (phoneBook.isStar) {
     // Импортируем из csv
--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -18,8 +18,50 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
-
+    if (!testAdd(phone, name, email)) {
+        return false;
+    }
+    phoneBook.push({ phone, name, email });
+    return true;
 };
+
+// Тесты на проверку при добавлении в справочник на
+// наличие имени 
+// чтобы не было совпадений номеров телефонов
+// костыльная проверка формата номера телефона 
+function testAdd(phone, name, email) {
+    if (typeof (name) === 'undefined') {
+
+        return false;
+    }
+    for (var str in phoneBook) {
+        if (phoneBook.hasOwnProperty(str)) {
+            var element = phoneBook[str];
+            if (element.phone.indexOf(phone) !== -1) {
+
+                return false;
+            }
+        }
+    }
+    if (typeof (phone) !== 'undefined') {
+        if (phone.length !== 10 || phone.match(/[^\d]/g, '')) {
+
+            return false;
+        }
+    }
+    if (!moreTest(phone) || !moreTest(name)) {
+        return false;
+    }
+
+    return true;
+}
+
+function moreTest(value) {
+    if (typeof (value) === 'undefined' || typeof (value) === '' || !value) {
+        return false;
+    }
+    return true;
+}
 
 /**
  * Обновление записи в телефонной книге
@@ -28,7 +70,22 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
+    if (moreTest(name) && moreTest(phone)) { //если имя задано, то обрабатывать
+        for (var str in phoneBook) {
+            if (phoneBook.hasOwnProperty(str)) {
+                var element = phoneBook[str];
+                if (element.phone.indexOf(phone) !== -1) {
+                    element.phone = phone;
+                    element.name = name;
+                    element.email = email;
+                }
+            }
+        }
 
+        return true;
+    }
+
+    return false;
 };
 
 /**
@@ -36,7 +93,43 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
+    let arr = [];
+    let index = 0;
+    let count = 0;
+    if (query === '*') {
+        let i = 0
+        for (i = 0; i <= phoneBook.length; i++) {
+            phoneBook.splice(0, 1);
+        }
+        return i;
+    }
+    for (var str in phoneBook) {
+        if (phoneBook.hasOwnProperty(str)) {
+            var element = phoneBook[str];
+            if (typeof (element.phone) !== 'undefined') {
+                if (element.phone.indexOf(query) !== -1) {
+                    arr.push(index);
+                }
+            }
+            if (typeof (element.name) !== 'undefined') {
+                if (element.name.indexOf(query) !== -1) {
+                    arr.push(index);
+                }
+            }
+            if (typeof (element.email) !== 'undefined') {
+                if (element.email.indexOf(query) !== -1) {
+                    arr.push(index);
+                }
+            }
+            index++;
+        }
+    }
+    arr.reverse();
+    arr.forEach(function (indexOfElem) {
+        phoneBook.splice(indexOfElem, 1);
+    }, this);
 
+    return arr.length;
 };
 
 /**
@@ -44,8 +137,61 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
+    let answer = [];
+    if (query === '' || !moreTest(query)) {
+        return [];
+    }
+    if (query === '*') {
+        for (var str in phoneBook) {
+            if (phoneBook.hasOwnProperty(str)) {
+                var element = phoneBook[str];
+                if (typeof (element.phone) !== 'undefined') {
+                    answer.push(sortAndExport(element));
+                }
+            }
+        }
+    }
 
+    if (typeof (query) === null || typeof (query) === 'undefined') {
+        return null;
+    }
+
+    for (var str in phoneBook) {
+        if (phoneBook.hasOwnProperty(str)) {
+            var element = phoneBook[str];
+            if (typeof (element.phone) !== 'undefined') {
+                if (element.phone.indexOf(query) !== -1) {
+                    answer.push(sortAndExport(element));
+                }
+            }
+            if (typeof (element.name) !== 'undefined') {
+                if (element.name.indexOf(query) !== -1) {
+                    answer.push(sortAndExport(element));
+                }
+            }
+            if (typeof (element.email) !== 'undefined') {
+                if (element.email.indexOf(query) !== -1) {
+                    answer.push(sortAndExport(element));
+                }
+            }
+        }
+    }
+    return answer.sort();
 };
+
+function sortAndExport(obj) {
+    let name = obj.name;
+    let phone = '+7 (' + obj.phone.slice(0, 3) + ') ' + obj.phone.slice(3, 6) + '-'
+        + obj.phone.slice(6, 8) + '-' + obj.phone.slice(8, 10);
+    let email;
+    if (typeof (obj.email) === 'undefined') {
+        email = '';
+    }
+    else
+        email = obj.email;
+    let o = name + ', ' + phone + ', ' + email;
+    return o;
+}
 
 /**
  * Импорт записей из csv-формата
