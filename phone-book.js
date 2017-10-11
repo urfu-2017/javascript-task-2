ffd3101e83ff644bf68214ebd68057683dc0bab3
Asmeev Alexander <daremove1@gmail.com>
2017-10-11 19:46:49 +0500

    test4

--- phone-book.js
+++ phone-book.js
@@ -74,7 +74,7 @@ exports.add = function (phone, name, email) {
 };
 
 exports.update = function (phone, name, email) {
-    if (!name || phoneBook[phone] {
+    if (!name || phoneBook[phone]) {
 
         return false;
     }
