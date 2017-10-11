4dd3fbf9b1b3f181abc4ddd080dd6240c6be34a4
Kraken7 <32228264+Kraken7@users.noreply.github.com>
2017-10-11 22:56:57 +0500

    1

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -18,7 +18,33 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
-
+	error = '';
+	if(!phone.match(/\b\d{10}\b/i)){
+		error = 1;
+	}
+	if(!email){
+		email = 0;
+	}
+	if(!name){
+		error = 1;
+	}
+	if(error){
+		return false;
+	}
+	else{
+		phoneBook.forEach(function(item, i, arr) {
+			if(item[0] == phone){
+				error = 1;
+			}
+		});
+		if(!error){
+			phoneBook[phoneBook.length] = [phone, name, email];
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 };
 
 /**
@@ -28,7 +54,29 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
-
+	error = '';
+	if(!phone.match(/\b\d{10}\b/i)){
+		error = 1;
+	}
+	if(!email){
+		email = 0;
+	}
+	if(!name){
+		error = 1;
+	}
+	if(error){
+		return false;
+	}
+	phoneBook.forEach(function(item, i, arr) {
+		if(item[0] == phone){
+			phoneBook[i][1] = name;
+			phoneBook[i][2] = email;
+			return true;
+		}
+		else{
+			return false;
+		}
+	});
 };
 
 /**
@@ -36,7 +84,18 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
-
+	if(query){
+		j = 0;
+		phoneBook.forEach(function(item, i, arr) {
+			item.forEach(function(item2, i2, arr2) {
+				if(item2 == query){
+					delete phoneBook[i];
+					j++;
+				}
+			});
+		});
+	}
+	return j;
 };
 
 /**
@@ -44,7 +103,25 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
-
+	findList = [];
+	if(query){
+		phoneBook.forEach(function(item, i, arr) {
+			item.forEach(function(item2, i2, arr2) {
+				if(item2 == query){
+					item[0] = '+7 (' + item[0].slice(0, 3) + ') ' + item[0].slice(3, 6) + '-' + item[0].slice(6, 8) + '-' + item[0].slice(8, 10);
+					findList[findList.length] = item[1] + ' ' + item[0] + ' ' + item[2];
+				}
+			});
+		});
+	}
+	if(query == "*"){
+		phoneBook.forEach(function(item, i, arr) {
+			item[0] = '+7 (' + item[0].slice(0, 3) + ') ' + item[0].slice(3, 6) + '-' + item[0].slice(6, 8) + '-' + item[0].slice(8, 10);
+			findList[findList.length] = item[1] + ' ' + item[0] + ' ' + item[2];
+		});
+	}
+	findList.sort();
+	return findList;
 };
 
 /**
