6fdcb9d9c76481cc5b0bfd0038314ec2dbbde718
art-ylem <art-ylem@mail.ru>
2017-10-12 10:55:18 +0300

    try1

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -17,7 +17,27 @@ var phoneBook;
  * @param {String} name
  * @param {String} email
  */
+
+var a = true;
+function isIncluded(phone) {
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i].phone === phone) {
+            a = false;
+        }
+
+    }
+
+    return a;
+}
 exports.add = function (phone, name, email) {
+    if (name !== '' && name !== undefined && name !== null && isIncluded(phone) &&
+    /^\d{10}$/.test(phone)) {
+        phoneBook.push({ phone: phone, name: name, email: email });
+
+        return true;
+    }
+
+    return false;
 
 };
 
@@ -27,24 +47,105 @@ exports.add = function (phone, name, email) {
  * @param {String} name
  * @param {String} email
  */
+function upd(phone, name, email, i) {
+    if (phoneBook[i].phone === phone && name !== undefined && name !== null && name !== '') {
+        phoneBook[i].name = name;
+        if (email !== undefined) {
+            phoneBook[i].email = email;
+        } else {
+            phoneBook[i].email = null;
+        }
+    }
+}
+
 exports.update = function (phone, name, email) {
-
-};
-
-/**
- * Удаление записей по запросу из телефонной книги
- * @param {String} query
- */
-exports.findAndRemove = function (query) {
-
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (upd(phone, name, email, i)) {
+            return true;
+        }
+    }
 };
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
  */
-exports.find = function (query) {
+var strphone;
+var strl = '';
+function strPhone(i) {
+    strphone = '+7 (' + phoneBook[i].phone.slice(0, 3) + ') ' +
+    phoneBook[i].phone.slice(3, 6) + '-' + phoneBook[i].phone.slice(6, 8) +
+    '-' + phoneBook[i].phone.slice(8, 10);
+    strl = phoneBook[i].name + ',' + strphone + ',' + phoneBook[i].email;
+}
 
+function funRegExp(query, list) {
+    for (var i = 0; i < phoneBook.length; i++) {
+        var re = new RegExp(query);
+        if (re.test(phoneBook[i].name) ||
+        re.test(phoneBook[i].phone) ||
+        re.test(phoneBook[i].eamil)) {
+            strPhone(i);
+            list.push(strl);
+        }
+    }
+}
+
+exports.find = function (query) {
+    var boola = false;
+    var list = [];
+    if (!query || query === '') {
+        list = [];
+        boola = false;
+    }
+    if (query === '*') {
+        for (var i = 0; i < phoneBook.length; i++) {
+            strPhone(i);
+            list.push(strl);
+        }
+        boola = true;
+    } else {
+        funRegExp(query, list);
+        if (list !== []) {
+            boola = true;
+        }
+
+    }
+
+    return boola;
+};
+
+/**
+ * Удаление записей по запросу из телефонной книги
+ * @param {String} query
+ */
+var count2 = 0;
+function findToDel(query) {
+    for (var i = 0; i < phoneBook.length; i++) {
+        var re = new RegExp(query);
+        if (re.test(phoneBook[i].name) ||
+        re.test(phoneBook[i].phone) ||
+        re.test(phoneBook[i].eamil)) {
+            phoneBook = phoneBook.splice(i, 1);
+            count2 += 1;
+        }
+    }
+
+    return count2;
+}
+exports.findAndRemove = function (query) {
+    var count = 0;
+    if (!query || query === '') {
+        count = 0;
+    } else if (query === '*') {
+        count = phoneBook.length;
+        phoneBook = phoneBook.splice(0, count);
+    } else {
+        findToDel(query);
+        count = count2;
+    }
+
+    return count;
 };
 
 /**
