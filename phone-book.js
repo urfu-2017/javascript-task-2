7e57b42b0935ec9ac0d753796b2078ff4ef0aeb0
Unknown <Danil.Koporushkin@urfu.me>
2017-10-13 00:20:54 +0500

    (

--- phone-book.js
+++ phone-book.js
@@ -37,7 +37,7 @@ function isItGood(phone, name, email) {
 
     return (((regExpPhone.test(phone) === true &&
         ((regExpEmail.test(email) === true) || email === undefined) &&
-        name !== undefined)) && !alreadyAdd(phone));
+        name !== undefined)) && !alreadyAdd(phone)) && !isNaN(Number(phone));
     // Object.keys(phoneBook).indexOf(phone) === -1);
 }
 function correctPhone(phone) {
@@ -48,7 +48,7 @@ function correctPhone(phone) {
 function isItGood2(phone, name, email) {
     return ((regExpPhone.test(phone) === true &&
         ((regExpEmail.test(email) === true) || email === undefined) &&
-        name !== undefined));
+        name !== undefined)) && !isNaN(Number(phone));
 }
 
 /**
